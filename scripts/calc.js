e20713574789bd75f61d7285970b18bc7ac82f3f
AlexVictorB <alexvictorbarreto@hotmail.com>
2021-06-28 07:53:40 -0300

    new files

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,30 +1,78 @@
+
+// Funções básicas da calculadora
+function DOMDisplay() {
+    let display = document.getElementById('display').value;
+
+    return display;
+}
+
 function insert(num) {
 
-    let number = document.getElementById('result').innerHTML;
+    let display = DOMDisplay();
 
-    document.getElementById("result").innerHTML = number + num;
+    document.getElementById('display').value = display + num
 
 }
 
 function clean() {
-    document.getElementById("result").innerHTML = "";
+    document.getElementById('display').value = "";
 }
 
 function back() {
 
-    let display = document.getElementById('result').innerHTML;
+    let display = document.getElementById('display').value;
 
-    document.getElementById('result').innerHTML = display.substring(0, display.length -1);
+    document.getElementById('display').value = display.substring(0, display.length -1);
 }
 
 function calc() {
 
-    let display = document.getElementById('result').innerHTML;
+    let display = document.getElementById('display').value;
 
     if(display) {
-        document.getElementById('result').innerHTML = eval(display)
+        document.getElementById('display').value = eval(display)
     }
     else {
-        document.getElementById('result').innerHTML = ""
+        document.getElementById('display').value = ""
     }
+}
+
+// Bloco para fazer teclas funcionarem
+
+let texto = document.getElementById('display');
+
+console.log(texto);
+
+texto.addEventListener("keypress", function (e) {
+    const keyCode = (e.keyCode ? e.keyCode : e.which);
+
+    console.log(keyCode)
+
+    //Exclui letras e mantém apenas números e simbolos
+
+    if (keyCode > 58) {
+        e.preventDefault();
+        insert(String.fromCharCode(keyCode))
+
+    }
+
+    if (keyCode < 41) {
+        e.preventDefault();
+        insert(String.fromCharCode(keyCode))
+
+    }
+
+    
+})
+
+
+function write() {
+
+    let texto = document.getElementById('display');
+
+    addEventListener("keypress", function(e){
+        const keyCode = (e.keyCode ? e.keyCode : e.which);
+
+        document.getElementById('display').value = "ola";
+    })
 }
